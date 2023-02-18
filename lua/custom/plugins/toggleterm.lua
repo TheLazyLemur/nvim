74f800417500b9b58acbe5cec5605220bd5b90a2
Dan Rousseau <danrousseau@protonmail.com>
2023-02-18 15:20:08 +0200

    feat: updated to lazy package manager

---
+++ lua/custom/plugins/toggleterm.lua
@@ -0,0 +1,12 @@
+return {
+  "akinsho/toggleterm.nvim",
+  config = function()
+    require('toggleterm').setup {
+      direction = 'float',
+      persist_size = false,
+      float_opts = {
+	border = 'curved'
+      }
+    }
+  end,
+}
