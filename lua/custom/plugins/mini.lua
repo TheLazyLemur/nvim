3b5fef45f862ef96210730a3001303a40ef7e0a4
Dan Rousseau <danrousseau@protonmail.com>
2023-03-13 10:19:27 +0200

    feat: added mini cursor words

---
+++ lua/custom/plugins/mini.lua
@@ -0,0 +1,20 @@
+return {
+	{
+		"echasnovski/mini.cursorword",
+		config = function()
+			require("mini.cursorword").setup()
+		end,
+	},
+	{
+		"echasnovski/mini.map",
+		config = function()
+			require("mini.map").setup()
+		end,
+	},
+	{
+		"echasnovski/mini.surround",
+		config = function()
+			require("mini.surround").setup()
+		end,
+	}
+}
