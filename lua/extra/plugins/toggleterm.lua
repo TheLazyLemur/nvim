1d98995a2cc9d0c5d1db4b4a362568844b051b05
danielrousseau <drousseau@luno.com>
2024-04-17 15:15:17 +0200

    Toggleterm config

--- lua/extra/plugins/toggleterm.lua
+++ lua/extra/plugins/toggleterm.lua
@@ -1,6 +1,19 @@
 return {
-    source = "akinsho/toggleterm.nvim",
-    config = function()
-        require("toggleterm").setup()
+  source = "akinsho/toggleterm.nvim",
+  config = function()
+    require("toggleterm").setup({
+      shade_terminals = false,
+      size = 20
+    })
+
+    local Terminal = require('toggleterm.terminal').Terminal
+    local lazygit  = Terminal:new({ hidden = true, height = 50 })
+
+    local function _toggle()
+      lazygit:toggle()
     end
+
+    vim.keymap.set("n", "=tt", _toggle, { noremap = true, silent = true })
+    vim.keymap.set("t", "=tt", _toggle, { noremap = true, silent = true })
+  end
 }
