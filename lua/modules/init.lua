a6d02ad8b1c1e7cbbb01ce8e218e1d02dfd3c187
danielrousseau <drousseau@luno.com>
2024-04-25 12:46:10 +0200

    chore: Remove module I created but never use, because telescope

--- lua/modules/init.lua
+++ lua/modules/init.lua
@@ -7,15 +7,6 @@ local modules = {
             vim.keymap.set("n", "<leader>bs", require("modules.bookmarks").show_selection_ui)
         end
     },
-    {
-        m = "modules.findit",
-        config = function()
-            require("modules.findit").setup()
-            vim.keymap.set("n", "=sg", function() require("modules.findit").grep() end)
-            vim.keymap.set("n", "=sf", function() require("modules.findit").find_files() end)
-            vim.keymap.set("n", "=sff", function() require("modules.findit").find_files(true) end)
-        end
-    },
 }
 
 for _, module in pairs(modules) do
