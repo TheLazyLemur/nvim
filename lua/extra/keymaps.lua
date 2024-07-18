7b834fffa59373bee3e6193ed15bdff9605d877a
danielrousseau <drousseau@luno.com>
2024-07-18 14:49:16 +0200

    Fix Trouble

--- lua/extra/keymaps.lua
+++ lua/extra/keymaps.lua
@@ -6,12 +6,7 @@ later(function()
   vim.keymap.set("n", "<leader>jj", function() require("flash").jump() end)
   vim.keymap.set("n", "<leader>jt", function() require("flash").treesitter() end)
 
-  vim.keymap.set("n", "<leader>xx", function() require("trouble").toggle() end)
-  vim.keymap.set("n", "<leader>xw", function() require("trouble").toggle("workspace_diagnostics") end)
-  vim.keymap.set("n", "<leader>xd", function() require("trouble").toggle("document_diagnostics") end)
-  vim.keymap.set("n", "<leader>xq", function() require("trouble").toggle("quickfix") end)
-  vim.keymap.set("n", "<leader>xl", function() require("trouble").toggle("loclist") end)
-  vim.keymap.set("n", "gR", function() require("trouble").toggle("lsp_references") end)
+  vim.keymap.set("n", "<leader>xx", ":Trouble diagnostics toggle<cr>")
 
   vim.keymap.set('n', '<F5>', dap.continue, { desc = 'Debug: Start/Continue' })
   vim.keymap.set('n', '<F1>', dap.step_into, { desc = 'Debug: Step Into' })
--- lua/extra/plugins/trouble.lua
+++ lua/extra/plugins/trouble.lua
@@ -3,13 +3,6 @@ return {
   lazy = true,
   depends = { "nvim-tree/nvim-web-devicons" },
   config = function()
-    require("trouble").setup({
-      auto_preview         = false,
-      padding              = false,
-      multiline            = false,
-      icons                = false,
-      use_diagnostic_signs = false,
-      severity             = vim.diagnostic.severity.ERROR,
-    })
+    require("trouble").setup()
   end,
 }
