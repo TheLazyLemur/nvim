3ce9495c278cdbfd6132e3d774ad488de46845fd
Dan Rousseau <danrousseau@protonmail.com>
2024-07-18 15:50:59 +0200

    Remove bad color settings

--- lua/extra/plugins/oxocarbon.lua
+++ lua/extra/plugins/oxocarbon.lua
@@ -3,7 +3,5 @@ return {
   config = function()
     vim.opt.background = "dark"
     vim.cmd.colorscheme "oxocarbon"
-    vim.api.nvim_set_hl(0, "Normal", { bg = "none" })
-    vim.api.nvim_set_hl(0, "NormalFloat", { bg = "none" })
   end
 }
