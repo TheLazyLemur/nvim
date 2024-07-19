b7e8c419952dc4c6beee92d8d66c08bc78537f5b
danielrousseau <drousseau@luno.com>
2024-07-19 12:22:06 +0200

    Moved dad bod config

--- lua/extra/plugins/vimdadbod.lua
+++ lua/extra/plugins/vimdadbod.lua
@@ -4,4 +4,13 @@ return {
     "kristijanhusak/vim-dadbod-completion",
     "kristijanhusak/vim-dadbod-ui",
   },
+  config = function()
+    local cmp = require "cmp"
+    cmp.setup.filetype({ "sql" }, {
+      sources = {
+        { name = "vim-dadbod-completion" },
+        { name = "buffer" },
+      }
+    })
+  end
 }
